98380c866e9724ce97b0ac41939857e3524aa455
kmponis <kmponis@gmail.com>
2020-06-03 01:54:23 +0100

    Platform-be - use both okhttp and resttemplate

--- platform-be/src/main/java/com/platform/be/restapi/service/impl/AwsApiAuthenticationServiceImpl.java
+++ platform-be/src/main/java/com/platform/be/restapi/service/impl/AwsApiAuthenticationServiceImpl.java
@@ -1,9 +1,7 @@
 package com.platform.be.restapi.service.impl;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.platform.be.restapi.model.Token;
 import com.platform.be.restapi.properties.ApplicationProperties;
 import com.platform.be.restapi.service.AwsApiAuthenticationService;
@@ -11,13 +9,15 @@ import com.squareup.okhttp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.ws.rs.BadRequestException;
-
-//import javax.ws.rs.core.MediaType;
+import java.util.Collections;
 
 @Service
 public class AwsApiAuthenticationServiceImpl implements AwsApiAuthenticationService {
@@ -29,7 +29,6 @@ public class AwsApiAuthenticationServiceImpl implements AwsApiAuthenticationServ
 
   private OkHttpClient okHttpClient = new OkHttpClient();
   private RestTemplate restTemplate = new RestTemplate();
-  private HttpHeaders httpHeaders;
 
   private static final String CONTENT_TYPE = "Content-Type";
   private static final String APPLICATION_JSON = "application/json";
@@ -48,21 +47,22 @@ public class AwsApiAuthenticationServiceImpl implements AwsApiAuthenticationServ
 
   @Override
   public Token authentication(String payload) {
-    RequestBody body = RequestBody.create(MediaType.parse(APPLICATION_JSON), payload);
     String uri = applicationProperties.baseUrl + "/authentication";
 
+    // okhttp
+    RequestBody body = RequestBody.create(MediaType.parse(APPLICATION_JSON), payload);
     Request request = new Request.Builder().url(uri).addHeader(CONTENT_TYPE, APPLICATION_JSON).post(body).build();
     JsonObject jsonObject = getJsonObject(request);
     if (null == jsonObject || null == jsonObject.get("token")) {
       throw new BadRequestException();
     }
     // RestTemplate
-//    JsonObject jsonObjectRT = getJsonObject(uri);
-//    if (null == jsonObjectRT || null == jsonObjectRT.get("token")) {
-//      throw new BadRequestException();
-//    }
+    JsonObject jsonObjectRT = getJsonObjectRT(uri, payload);
+    if (null == jsonObjectRT || null == jsonObjectRT.get("token")) {
+      throw new BadRequestException();
+    }
 
-    Token token = new Gson().fromJson(jsonObject.get("token").getAsJsonObject(), Token.class);
+    Token token = new Gson().fromJson(jsonObjectRT.get("token").getAsJsonObject(), Token.class);
     return token;
   }
 
@@ -75,9 +75,7 @@ public class AwsApiAuthenticationServiceImpl implements AwsApiAuthenticationServ
   private JsonObject getJsonObject(Request request) {
     try {
       Response response = okHttpClient.newCall(request).execute();
-      JsonParser parser = new JsonParser();
-      JsonElement jsonElement = parser.parse(response.body().string());
-      JsonObject jsonResponse = jsonElement.getAsJsonObject();
+      JsonObject jsonResponse = new Gson().fromJson(response.body().string(), JsonObject.class);
       logger.info("Endpoint: {} - response: {}", request.httpUrl(), jsonResponse);
       return jsonResponse;
     } catch (Exception e) {
@@ -92,15 +90,14 @@ public class AwsApiAuthenticationServiceImpl implements AwsApiAuthenticationServ
    * @param uri
    * @return jsonResponse
    */
-  private JsonObject getJsonObject(String uri) {
+  private JsonObject getJsonObjectRT(String uri, String payload) {
     try {
-//      HttpHeaders headers = new HttpHeaders();
-//      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//      HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-//      ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-//      JsonObject jsonResponse = new Gson().fromJson(result.getBody(), JsonObject.class);
+      HttpHeaders headers = new HttpHeaders();
+      headers.setAccept(Collections.singletonList(org.springframework.http.MediaType.APPLICATION_JSON));
+      HttpEntity<String> entity = new HttpEntity<>(payload, headers);
+      ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+      JsonObject jsonResponse = new Gson().fromJson(result.getBody(), JsonObject.class);
 
-      JsonObject jsonResponse = restTemplate.getForObject(uri, JsonObject.class);
       logger.info("Endpoint: {} - response: {}", uri, jsonResponse);
       return jsonResponse;
     } catch (Exception e) {
